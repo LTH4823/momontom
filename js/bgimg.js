65c24c753e38871c32fd0061e72bd23a9b5ff5c4
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2021-02-21 17:09:18 +0900

    Update bgimg.js

--- js/bgimg.js
+++ js/bgimg.js
@@ -4,7 +4,7 @@ IMG_NUMBER = 3;
 function paintImage(imgNumber){
     main.classList.add("bgimg");
     const bgimg = document.querySelector(".bgimg");
-    bgimg.style.backgroundImage = `url(../img/img${imgNumber+1}.jpg)`;
+    bgimg.style.backgroundImage = `url(/img/img${imgNumber+1}.jpg)`;
     bgimg.style.backgroundSize = "cover";
     bgimg.style.backgroundPosition = "center";
     console.log(imgNumber);
